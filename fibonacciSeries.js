9e7deeed99d73967b6b948f1a0b668f187c242ec
Maalaiammal Elangovan <56559609+MaalaiammalElangovan@users.noreply.github.com>
2026-02-01 18:14:28 +0530

    Add files via upload

---
+++ fibonacciSeries.js
@@ -0,0 +1,13 @@
+function fibonacciSequence(n) {
+    let result = [];
+    if (n === 1) {
+        result.push(0, 1);
+    } else {
+        result = fibonacciSequence(n - 1);
+        result.push(result[result.length - 1] + result[result.length - 2]);
+    }
+    return result;
+}
+
+let n = 10;
+console.log(fibonacciSequence(n));
